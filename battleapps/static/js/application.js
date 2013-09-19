532dd49e7a502df77db4090866b97cbbfeb17805
Kevin Cherepski <kevinc@dreamworldpartners.com>
2013-09-18 22:34:01 -0700

    another commit

--- battleapps/static/js/application.js
+++ battleapps/static/js/application.js
@@ -5,3 +5,16 @@ App.ApplicationRoute = Ember.Route.extend({
     return Ember.$.getJSON('http://www.bootybattleapp.com/api/v1/profile/?format=json');
   }
 });
+
+App.jQueryCarouselView = Ember.View.extend({
+    didInsertElement: function() {
+        $(function() {
+            $('.jcarousel_left').jcarousel({
+            // Configuration goes here
+            });
+            $('.jcarousel_right').jcarousel({
+            // Configuration goes here
+            });
+        });
+    }
+});
